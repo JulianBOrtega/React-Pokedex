ad01d182b49ea634363a14e3ee97118d09ee2329
Julián <j.gakusei11@gmail.com>
2023-05-23 03:13:43 -0300

    go to top button added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { PokemonGallery } from '../components/PokemonGallery'
 import { Searchbar } from '../components/Searchbar'
+import './home.css';
+
 export const Home = () => {
   return (
-    <div>
+    <div className='homePage'>
       <br />{/* Los br sirven, te gusten o no */}<br />
       <Searchbar/>
       <PokemonGallery/>
+      <a href='#' className='goUp'>↑</a>
     </div>
   )
 }
